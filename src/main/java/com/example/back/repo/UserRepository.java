cae339775f4729d9cf25974d037b72e7f9df8c23
Wei Ding <ding_web@yahoo.co.jp>
2021-06-06 05:29:58 -0400

    set interface

--- src/main/java/com/example/back/repo/UserRepository.java
+++ src/main/java/com/example/back/repo/UserRepository.java
@@ -3,5 +3,9 @@ package com.example.back.repo;
 /**
  * Created by ding_ on 6/6/2021
  */
-public class UserRepository {
+
+import com.example.back.models.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
 }
