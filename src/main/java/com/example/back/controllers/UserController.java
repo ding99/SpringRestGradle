e2eea0a85418e822041f1ee920b7206027d7d881
Wei Ding <ding_web@yahoo.co.jp>
2021-06-06 05:34:31 -0400

    implement end points

--- src/main/java/com/example/back/controllers/UserController.java
+++ src/main/java/com/example/back/controllers/UserController.java
@@ -1,7 +1,45 @@
 package com.example.back.controllers;
 
+import com.example.back.models.User;
+import com.example.back.repo.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
 /**
  * Created by ding_ on 6/6/2021
  */
+@RestController
+@RequestMapping("/api/user")
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> findAllUsers(){
+        List<User> users = (List<User>) userRepository.findAll();
+        return users;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findUserById(@PathVariable(value = "id") long id){
+        Optional<User> user = userRepository.findById(id);
+
+        if(user.isPresent()){
+            return ResponseEntity.ok().body(user.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping()
+    public User saveUser(@Validated @RequestBody User user){
+        return userRepository.save(user);
+    }
 }
